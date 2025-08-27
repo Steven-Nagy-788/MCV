40371c396790baa58c2233790a984820974a85dd
Steven Nagy <stevennagy788@gmail.com>
2025-08-27 20:39:15 +0300

    4

---
+++ backendTasks/#4_task_swagger/taskSwag/taskSwag/Migrations/20250827132523_adding sec table.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace taskSwag.Migrations
+{
+    /// <inheritdoc />
+    public partial class addingsectable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LocationId",
+                table: "Users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Countery = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_LocationId",
+                table: "Users",
+                column: "LocationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Locations_LocationId",
+                table: "Users",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Locations_LocationId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_LocationId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LocationId",
+                table: "Users");
+        }
+    }
+}
---
+++ backendTasks/#4_task_swagger/taskSwag/taskSwag/Models/Location.cs
@@ -0,0 +1,13 @@
+﻿namespace taskSwag.Models
+{
+    public class LocationDto
+    {
+        public string? City { get; set; }
+        public string? Countery { get; set; }
+    }
+    public class Location : LocationDto
+    {
+        public int? Id { get; set; }
+        public List<User>? Users { get; set; }
+    }
+}
